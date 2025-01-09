4e4918b245996657e29eab76b26c6b8a2bcbaa3b
Jakob Schlyter <jakob@kirei.se>
2025-01-09 06:29:10 +0100

    Remove unused comments

--- pypolestar/exceptions.py
+++ pypolestar/exceptions.py
@@ -1,6 +1,3 @@
-"""Exceptions for Polestar API."""
-
-
 class PolestarApiException(Exception):
     """Base class for exceptions in this module."""
 
@@ -12,7 +9,6 @@ class PolestarAuthException(PolestarApiException):
     message: str | None = None
 
     def __init__(self, message: str, error_code: int | None = None) -> None:
-        """Initialize the Polestar API."""
         super().__init__(message)
         self.error_code = error_code
 
